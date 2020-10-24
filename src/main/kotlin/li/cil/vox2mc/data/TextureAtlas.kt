f7f33e77b8a27137b5fc06ec0f81e7ea13406035
Florian Nücke <florian@nuecke.de>
2020-10-24 12:34:57 +0200

    moved atlas to own file

---
+++ src/main/kotlin/li/cil/vox2mc/data/TextureAtlas.kt
@@ -0,0 +1,59 @@
+package li.cil.vox2mc.data
+
+class TextureAtlas(private val size: Int2) {
+    constructor(size: Int) : this(Int2(size, size))
+
+    private var data: Data? = null
+
+    fun faces(): Sequence<BlockFace> = data?.let {
+        return (it.child0?.faces() ?: emptySequence()) +
+                (it.child1?.faces() ?: emptySequence()) +
+                it.face
+    } ?: emptySequence()
+
+    fun add(face: BlockFace): Boolean {
+        if (face.size().x > size.x || face.size().y > size.y) {
+            return false
+        }
+        data?.let { data ->
+            if (face.size().y >= face.size().y) {
+                data.child0?.let { if (it.add(face)) return true }
+                data.child1?.let { if (it.add(face)) return true }
+            } else {
+                data.child1?.let { if (it.add(face)) return true }
+                data.child0?.let { if (it.add(face)) return true }
+            }
+            return false
+        }
+
+        val faceSize = face.size()
+        if (faceSize.x > size.x || faceSize.y > size.y) {
+            return false
+        }
+
+        val sizeChild0 = Int2(size.x - faceSize.x, faceSize.y)
+        val child0 = if (sizeChild0 != Int2.ZERO) TextureAtlas(sizeChild0) else null
+        val sizeChild1 = Int2(size.x, size.y - faceSize.y)
+        val child1 = if (sizeChild1 != Int2.ZERO) TextureAtlas(sizeChild1) else null
+        data = Data(face, child0, child1)
+        return true
+    }
+
+    fun applyUVs() {
+        applyUVs(0, 0, size.x.toFloat(), size.y.toFloat())
+    }
+
+    private fun applyUVs(u0: Int, v0: Int, sizeU: Float, sizeV: Float) {
+        data?.let { data ->
+            val faceSize = data.face.size()
+            data.face.uvs = arrayOf(
+                u0 / sizeU, v0 / sizeV,
+                (u0 + faceSize.x) / sizeU, (v0 + faceSize.y) / sizeV
+            )
+            data.child0?.applyUVs(u0 + faceSize.x, v0, sizeU, sizeV)
+            data.child1?.applyUVs(u0, v0 + faceSize.y, sizeU, sizeV)
+        }
+    }
+
+    private data class Data(val face: BlockFace, val child0: TextureAtlas?, val child1: TextureAtlas?)
+}
