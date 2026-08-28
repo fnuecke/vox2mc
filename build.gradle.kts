f0f9ec9a28ddd7408666064f48213f182780489e
Florian Nücke <florian@nuecke.de>
2026-08-28 12:59:09 +0200

    Prep publishing flow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     kotlin("jvm") version "2.2.20"
     application
+    `maven-publish`
 }
 
+val semver: String by project
+
 group = "li.cil.vox2mc"
-version = "0.1.0"
+version = semver
 
 kotlin {
     jvmToolchain(21)
@@ -24,4 +27,44 @@ dependencies {
 
 application {
     mainClass = "li.cil.vox2mc.MainKt"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name = "vox2mc"
+                description = "Turns MagicaVoxel models into Minecraft block models, one quad soup and one sprite each."
+                url = "https://github.com/fnuecke/vox2mc"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://github.com/fnuecke/vox2mc/blob/main/LICENSE"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "fnuecke"
+                        name = "Florian Nücke"
+                    }
+                }
+                scm {
+                    connection = "scm:git:https://github.com/fnuecke/vox2mc.git"
+                    developerConnection = "scm:git:ssh://git@github.com/fnuecke/vox2mc.git"
+                    url = "https://github.com/fnuecke/vox2mc"
+                }
+            }
+        }
+    }
+    repositories {
+        val mavenRepoDir = (project.findProperty("mavenRepoDir") as String?)?.takeIf { it.isNotBlank() }
+        if (mavenRepoDir != null) {
+            maven {
+                name = "StaticMavenRepo"
+                url = uri(file(mavenRepoDir))
+            }
+        }
+    }
 }
